1bc250b35350e2d8b290a55644a05040f26ff646
Yarin Rahamim <yarinrahamim5@gmail.com>
2022-11-06 18:54:29 +0200

    third upload

--- Code1.c
+++ Code1.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
 int main() {
-   printf("Hello, World! My name is Yarin Rahamim");
+   printf("Hello, World! My name is Yarin Rahamim I was started DevOps course");
    return 0;
 }
